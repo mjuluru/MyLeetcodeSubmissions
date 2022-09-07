e4ff0f62235e1547b1e63b561e5f25a880be0af1
Mamatha Juluru <90650424+mjuluru@users.noreply.github.com>
2022-09-07 08:12:45 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,11 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums = sorted(enumerate(nums), key = lambda t:t[1])
-        l,r = 0, len(nums)-1
-        while l<=r :
-            if nums[l][1]+nums[r][1] < target:
-                l += 1
-            elif nums[l][1]+nums[r][1] > target:
-                r -= 1
-            else:
-                return nums[l][0],nums[r][0]
+        for n in range(0, len(nums)):
+            if (target-nums[n]) in nums:
+                idx = nums.index(target-nums[n])
+                if idx != n:
+                    return [n, idx]
