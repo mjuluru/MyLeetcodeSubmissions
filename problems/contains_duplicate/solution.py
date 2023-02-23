d34335f9105f26cd09b55ff5f684f62db4579ebf
Mamatha Juluru <90650424+mjuluru@users.noreply.github.com>
2023-02-23 20:15:57 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

--- problems/contains_duplicate/solution.py
+++ problems/contains_duplicate/solution.py
@@ -2,9 +2,8 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         count = {}
         for i in nums:
-            if i in count.keys():
+            if i in count:
                 return True
-                break
             else:
                 count[i] = 1
         return False
